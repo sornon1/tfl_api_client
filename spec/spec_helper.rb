b66f87fdf48366f886e649ef84b94c192dcacd8a
Luke Hackett <Luke.Hackett@live.co.uk>
2015-08-04 21:52:16 +0100

    Added Fuubar for a prettier test console output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -88,8 +88,12 @@ RSpec.configure do |config|
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
+    # config.default_formatter = 'doc'
   end
+
+  # Use Fuubar to handle pretty printing of the test suite
+  config.default_formatter = 'Fuubar'
+  config.color = true
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
--- tfl_api_client.gemspec
+++ tfl_api_client.gemspec
@@ -28,6 +28,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'yard', '~> 0.8.7'
 
   spec.add_development_dependency 'rspec', '~> 3.3'
+  spec.add_development_dependency 'fuubar', '~> 2.0'
   spec.add_development_dependency 'vcr', '~> 2.9'
   spec.add_development_dependency 'webmock', '~> 1.21'
   spec.add_development_dependency 'simplecov', '~> 0.10'
